098da427e046d6a44083d2b75eeaf777b661431f
mnmami <mohamed.nadjib.mami@gmail>
2017-08-08 14:50:50 +0200

    first push of all files

---
+++ src/main/scala/Sparking.scala
@@ -0,0 +1,88 @@
+import com.mongodb.spark.config.ReadConfig
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.collection.mutable.{HashMap, Set}
+
+/**
+  * Created by mmami on 30.01.17.
+  */
+
+import org.apache.log4j.{Level, Logger}
+
+class Sparking(sparkURI: String) {
+
+    def query (sources : Set[(HashMap[String, String], String, String)], optionsMap: HashMap[String, Map[String, String]], toJoinWith: Boolean): DataFrame = {
+
+        Logger.getLogger("org").setLevel(Level.OFF)
+        Logger.getLogger("akka").setLevel(Level.OFF)
+
+        val spark = SparkSession.builder.master(sparkURI).appName("Sparkall").getOrCreate;
+
+        var finalDF : DataFrame = null
+        var datasource = 0
+
+        for (s <- sources) {
+            println("*****************************************************")
+            datasource += 1 // in case of multiple relevant data sources to union
+
+            var attr_pred = s._1
+            var sourcePath = s._2
+            var sourceType = Helpers.getTypeFromURI(s._3)
+            var options = optionsMap(sourcePath)
+
+            var columns = Helpers.getSelectColumnsFromSet(attr_pred)
+
+            if(toJoinWith) {
+                println(datasource + ") Relevant source: " + sourcePath)
+                var id = Helpers.getID(sourcePath)
+                println("=> Whose ID (from RML subjectMap) is: " + id)
+                columns = columns + "," + id + " AS ID"
+            }
+
+            println("Getting the columns: " + columns + " from the source: " + sourcePath + " of type: " + sourceType + " with the options: " + options)
+
+            var df : DataFrame = null
+            sourceType match {
+                case "csv" => df = spark.read.options(options).csv(sourcePath)
+                case "parquet" => df = spark.read.options(options).parquet(sourcePath)
+                case "cassandra" =>
+                    //spark.conf.set("spark.cassandra.connection.host", "127.0.0.1")
+                    //println("CASSANDRACONF:" + spark.conf.get("spark.cassandra.connection.host"))
+                    df = spark.read.format("org.apache.spark.sql.cassandra").options(options).load
+                case "mongodb" =>
+                    //spark.conf.set("spark.mongodb.input.uri", "mongodb://127.0.0.1/test.myCollection")
+                    val vals = options.values.toList
+                    val mongoConf = Helpers.makeMongoURI(vals(0), vals(1), vals(2))
+                    val mongoOptions: ReadConfig = ReadConfig(Map("uri" -> mongoConf))
+                    df = spark.read.format("com.mongodb.spark.sql").options(mongoOptions.asOptions).load
+                case _ =>
+            }
+
+            df.createOrReplaceTempView("table")
+            var newDF = spark.sql("SELECT " + columns + " FROM table")
+
+            if(datasource == 1) {
+                finalDF = newDF
+
+                newDF.show()
+
+                //finalDF.show()
+                //println("-Print before union ")
+                //finalDF.collect.foreach(t => println(t))
+                //finalDF.printSchema
+            } else {
+                finalDF = finalDF.union(newDF)
+
+                //finalDF.show()
+                //println("-Print after union ")
+                //finalDF.collect.foreach(t => println(t))
+                //finalDF.printSchema
+            }
+
+            //println("2")
+            //finalDF.printSchema()
+        }
+
+        return finalDF
+    }
+}
